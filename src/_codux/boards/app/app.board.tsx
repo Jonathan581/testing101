9ed995d9fd8fceaa28045301a0a946993490612e
Jonathan581 <72512616+Jonathan581@users.noreply.github.com>
2023-11-16 13:19:52 +0300

    sec commit

--- src/_codux/boards/app/app.board.tsx
+++ src/_codux/boards/app/app.board.tsx
@@ -1,23 +1,8 @@
-import '../../../index.css';
 import { createBoard } from '@wixc3/react-board';
 import App from '../../../App';
 
 export default createBoard({
     name: 'App',
-    Board: () => <div>
-        <div />
-        <div className="welcome-section">
-            <h2>Heading 2</h2>
-        </div>
-        <div>
-            <div>
-                <button>Button</button>
-            </div>
-        </div>
-        <div />
-    </div>,
-    environmentProps: {
-        windowWidth: 1024,
-        windowHeight: 768
-    }
+    Board: () => <App />,
+    isSnippet: true,
 });
