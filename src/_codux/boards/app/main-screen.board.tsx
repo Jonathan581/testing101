7a6f0b4b50b149cf5e203428a27f498d84b6a1df
Jonathan581 <72512616+Jonathan581@users.noreply.github.com>
2023-11-16 13:17:44 +0300

    first commit

--- src/_codux/boards/app/main-screen.board.tsx
+++ src/_codux/boards/app/main-screen.board.tsx
@@ -3,21 +3,14 @@ import { createBoard } from '@wixc3/react-board';
 import App from '../../../App';
 
 export default createBoard({
-    name: 'App',
+    name: 'Main screen',
     Board: () => <div>
         <div />
         <div className="welcome-section">
             <h2>Heading 2</h2>
         </div>
-        <div>
-            <div>
-                <button>Button</button>
-            </div>
-        </div>
+        <div />
         <div />
     </div>,
-    environmentProps: {
-        windowWidth: 1024,
-        windowHeight: 768
-    }
+    isSnippet: true,
 });
